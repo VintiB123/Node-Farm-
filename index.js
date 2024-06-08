e9d1fffd2f086f2fd6205061f36dbfd719ab0e75
VintiB123 <vintibhatia98@gmail.com>
2024-06-08 11:51:30 +0530

    almost done with first node project

--- index.js
+++ index.js
@@ -2,26 +2,10 @@ const fs = require("fs");
 const http = require("http");
 const path = require("path");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplates");
 
 //SERVER
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%/g, "not-organic");
-  } else {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "");
-  }
-  return output;
-};
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -51,10 +35,9 @@ const server = http.createServer((req, res) => {
     res.end(output);
   }
   //PRODUCT PAGE
-  else if (pathname === "/product") {
+  else if (pathname === `/product`) {
     res.writeHead(200, { "Content-type": "text/html" });
     const product = dataObj[query.id];
-    console.log(product);
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
   }
